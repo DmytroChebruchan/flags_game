f9bc00ba9005bfbd8a649e4b042f7b035a531001
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-12-04 23:22:39 +0200

    diabling america

--- flag_quest/tests.py
+++ flag_quest/tests.py
@@ -16,3 +16,14 @@ class TestViews(TestCase):
         response = self.client.get(reverse('list_of_countries'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'flag_quest/list_of_countries.html')
+
+    def test_results_countries_view(self):
+        response = self.client.get(reverse('results'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'flag_quest/results.html')
+
+    def test_game_page_view_get(self):
+        response = self.client.get(
+            reverse('game', kwargs={'continent_name': 'Europe'}))
+        self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'flag_quest/flag_quest.html')
