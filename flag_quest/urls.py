6e3113acbd6284d1fbeb00d0732236eac84bcb7e
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-12-05 17:26:15 +0200

    enabeling options of North and South America

--- flag_quest/urls.py
+++ flag_quest/urls.py
@@ -4,7 +4,7 @@ from .views import GamePage, IndexView, ListCounties, ResultsCountries
 
 urlpatterns = [
     path("list_of_countries", ListCounties.as_view(), name="list_of_countries"),
-    path("game/<slug:continent_name>", GamePage.as_view(), name="game"),
+    path("game/<path:continent_name>", GamePage.as_view(), name="game"),
     path("results", ResultsCountries.as_view(), name="results"),
     path("", IndexView.as_view(), name="index"),
 ]
