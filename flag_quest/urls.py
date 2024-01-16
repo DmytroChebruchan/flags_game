379078738178259646c4d8cca8dcf4eb11175f15
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-01-16 16:55:52 +0200

    reformating and country details url was added

--- flag_quest/urls.py
+++ flag_quest/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 
-from .views import GamePage, IndexView, ListCounties, ResultsCountries
+from .views import (
+    CountryDetailsView,
+    GamePage,
+    IndexView,
+    ListCounties,
+    ResultsCountries,
+)
 
 urlpatterns = [
     path("list_of_countries/", ListCounties.as_view(), name="all_countries"),
-    path("list_of_countries/<str:continent>", ListCounties.as_view(),
-         name="countries_by_continent"),
+    path(
+        "list_of_countries/<str:continent>",
+        ListCounties.as_view(),
+        name="countries_by_continent",
+    ),
+    path(
+        "coutry_details/<slug:country>",
+        CountryDetailsView.as_view(),
+        name="country_details",
+    ),
     path("game/", GamePage.as_view(), name="game"),
     path("game/<path:continent_name>", GamePage.as_view(), name="game"),
     path("results", ResultsCountries.as_view(), name="results"),
