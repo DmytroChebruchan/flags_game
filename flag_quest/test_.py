250897f7a3d017bd3e4a51a13f2c7ce17d8f2367
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-01-22 17:51:11 +0200

    adding working tests only

--- flag_quest/test_.py
+++ flag_quest/test_.py
@@ -2,9 +2,6 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 
-# from flag_quest.models import Answer, CountryInfo
-
-
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
@@ -17,29 +14,9 @@ class TestViews(TestCase):
     def test_results_view(self):
         response = self.client.get(reverse("results"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response,
-                                "flag_quest/results.html")
-        
+        self.assertTemplateUsed(response, "flag_quest/results.html")
+
     def test_about_view(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "flag_quest/about.html")
-
-    # def test_list_countries_view(self):
-    #     # Update the URL to include the continent parameter
-    #     url = reverse("countries_by_continent", args=["Europe"])
-    #     response = self.client.get(url)
-    #
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "flag_quest/list_of_countries.html")
-    #
-    # def test_results_countries_view(self):
-    #     response = self.client.get(reverse("results"))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "flag_quest/results.html")
-    #
-    # def test_game_page_view_get(self):
-    #     response = self.client.get(
-    #         reverse("game", kwargs={"continent_name": "Europe"}))
-    #     self.assertEqual(response.status_code, 200)
-    #     # self.assertTemplateUsed(response, 'flag_quest/flag_quest.html')
