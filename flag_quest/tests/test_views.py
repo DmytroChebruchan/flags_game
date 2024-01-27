60ac86e4af89106715cf5d043c61ce35fc60ef89
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-01-27 22:26:26 +0200

    reformatting

--- flag_quest/tests/test_views.py
+++ flag_quest/tests/test_views.py
@@ -9,14 +9,14 @@ class TestViews(TestCase):
     def test_index_view(self):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "index.html")
+        self.assertEqual(response.template_name[0], "index.html")
 
     def test_results_view(self):
         response = self.client.get(reverse("results"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "flag_quest/results.html")
+        self.assertEqual(response.template_name[0], "flag_quest/results.html")
 
     def test_about_view(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "flag_quest/about.html")
+        self.assertEqual(response.template_name[0], "flag_quest/about.html")
