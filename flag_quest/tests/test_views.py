0824410a06cc88e84f0fe91035c2fe0605324131
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-01-28 17:27:38 +0200

    tests of few pages were added

--- flag_quest/tests/test_views.py
+++ flag_quest/tests/test_views.py
@@ -2,7 +2,8 @@ from django.http import Http404
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from flag_quest.models import CountryInfo
+from flag_quest.models import CountryInfo, Answer
+from flag_quest.tests.additional_functions import dummy_answers_creator
 from flag_quest.views import CountryDetailsView
 
 
@@ -60,3 +61,47 @@ class TestDetailsView(TestCase):
 
         # Check that the returned object matches the created country
         self.assertEqual(obj, country)
+
+
+class TestResultsCountriesView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_one_page_results_countries_get(self):
+        dummy_answers_creator(2)
+        response = self.client.get(reverse("results"))
+        self.assertTrue("results" in response.context_data)
+        self.assertEqual(len(response.context_data["results"]),
+                         2)
+
+    def test_few_pages_results_countries_get(self):
+        dummy_answers_creator(10)
+        response = self.client.get(reverse("results"))
+        self.assertEqual(len(response.context_data["results"]),
+                         5)
+
+        # Additional checks on context data if needed
+
+    def test_results_countries_post(self):
+        # Create sample answer objects
+        Answer.objects.create(
+            flag_picture="test_flag_picture_1",
+            is_correct=True,
+            your_answer="correct_answer_1",
+            correct_answer="correct_answer_1"
+        )
+        Answer.objects.create(
+            flag_picture="test_flag_picture_2",
+            is_correct=False,
+            your_answer="incorrect_answer_2",
+            correct_answer="correct_answer_2"
+        )
+
+        # Make POST request to clean answers
+        response = self.client.post(reverse("results"), {"clean": "clean"})
+
+        # Check redirection
+        self.assertRedirects(response, reverse("results"))
+
+        # Check if all Answer objects are deleted
+        self.assertEqual(Answer.objects.count(), 0)
