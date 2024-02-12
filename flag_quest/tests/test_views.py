b1318ca28b78595f0f9bd5a8e07039bf75fc287f
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-02-12 20:19:53 +0200

    devided tests of status code and template used

--- flag_quest/tests/test_views.py
+++ flag_quest/tests/test_views.py
@@ -13,24 +13,21 @@ from flag_quest.views import CountryDetailsView, GamePage
 django.setup()
 
 
-class TestViewsOnPing(TestCase):
+class TestViewsStatusCode(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_index_view(self):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name[0], "index.html")
 
     def test_results_view(self):
         response = self.client.get(reverse("results"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name[0], "flag_quest/results.html")
 
     def test_about_view(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name[0], "flag_quest/about.html")
 
     def test_details_view(self):
         self.country1 = CountryInfo.objects.create(name="test_country")
@@ -38,9 +35,31 @@ class TestViewsOnPing(TestCase):
             reverse("country_details", kwargs={"country": "test_country"})
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.template_name[0], "flag_quest/country_details.html"
+
+
+class TestViewsTemplates(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_template(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.template_name[0], "index.html")
+
+    def test_results_template(self):
+        response = self.client.get(reverse("results"))
+        self.assertEqual(response.template_name[0], "flag_quest/results.html")
+
+    def test_about_template(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.template_name[0], "flag_quest/about.html")
+
+    def test_details_template(self):
+        self.country1 = CountryInfo.objects.create(name="test_country")
+        response = self.client.get(
+            reverse("country_details", kwargs={"country": "test_country"})
         )
+        self.assertEqual(response.template_name[0],
+                         "flag_quest/country_details.html")
 
 
 class TestDetailsView(TestCase):
